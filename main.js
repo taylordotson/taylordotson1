190402cea7e26f534153c13907bbb12c6a8af5f0
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-07-20 19:30:14 -0500

    more progress

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+
+//NAVBAR//
+
+$(window).scroll(function () {
+    // 100 = The point you would like to fade the nav in.
+
+    if ($(window).scrollTop() > 100) {
+
+        $('.bg').addClass('show');
+
+    } else {
+
+        $('.bg').removeClass('show');
+
+    };
+});
+
+$('.scroll').on('click', function (e) {
+    e.preventDefault()
+
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1500);
+});
+
+///////
+
+
 var parallaxElements = [];
 var windowHeight = 0;
 var speed = 2;
